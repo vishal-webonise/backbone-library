263e31b1f6793b8964a8ef4963c9b26fde49005e
Vishal Telangre <the@vishaltelangre.com>
2013-10-27 22:33:30 +0530

    add functionality to add books via form

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -3,9 +3,31 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
   el: '#books',
 
+  events: {
+    'click #add': 'addBook'
+  },
+
   initialize: function   ( initialBooks ) {
     this.collection = new app.Library( initialBooks );
     this.render();
+
+    this.listenTo( this.collection, 'add', this.renderBook );
+  },
+
+  // create book model and render the view for it from the form
+  // submitted data
+  addBook: function( event ) {
+    event.preventDefault();
+
+    var formData = {};
+
+    $('#addBook div').children('input').each(function( i, el ){
+      if ( $( el ).val() !== '' ) {
+        formData[ el.id ] = $( el ).val();
+      }
+    });
+
+    this.collection.add( new app.Book( formData ) );
   },
 
   // render library by rendering each book in its collection
